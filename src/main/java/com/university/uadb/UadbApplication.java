4f972b40211972ebcf2f907c315fa439b377a8c6
Falilou66 <fadelvlt29@gmail.com>
2025-12-19 15:09:02 +0000

    cours 1

---
+++ src/main/java/com/university/uadb/UadbApplication.java
@@ -0,0 +1,14 @@
+package com.university.uadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UadbApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/university/uadb/UadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.uadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
